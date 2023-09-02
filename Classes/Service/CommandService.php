63f85bc9faec1cd6f7fec7f8ffa4a8ca3d9f0643
Andreas Nedbal <andreas.nedbal@in2code.de>
2023-09-02 16:28:22 +0200

    [TASK] Properly namespace CommandService

--- Classes/Service/CommandService.php
+++ Classes/Service/CommandService.php
@@ -1,4 +1,11 @@
 <?php
+
+namespace Caretaker\CaretakerInstance\Service;
+
+use Caretaker\CaretakerInstance\Entity\Command\CommandRequest;
+use Caretaker\CaretakerInstance\Entity\Command\CommandResult;
+use Caretaker\CaretakerInstance\Exception\SecurityManagerException;
+
 /***************************************************************
  * Copyright notice
  *
@@ -48,25 +55,25 @@
  * @author Tobias Liebig <liebig@networkteam.com>
  *
  */
-class tx_caretakerinstance_CommandService
+class CommandService
 {
     /**
-     * @var tx_caretakerinstance_ISecurityManager
+     * @var ISecurityManager
      */
     protected $securityManager;
 
     /**
-     * @var tx_caretakerinstance_OperationManager
+     * @var OperationManager
      */
     protected $operationManager;
 
     /**
      * Construct a new Command Service
      *
-     * @param tx_caretakerinstance_OperationManager $operationManager
-     * @param tx_caretakerinstance_ISecurityManager $securityManager
+     * @param OperationManager $operationManager
+     * @param ISecurityManager $securityManager
      */
-    public function __construct(tx_caretakerinstance_OperationManager $operationManager, tx_caretakerinstance_ISecurityManager $securityManager)
+    public function __construct(OperationManager $operationManager, ISecurityManager $securityManager)
     {
         $this->operationManager = $operationManager;
         $this->securityManager = $securityManager;
@@ -77,10 +84,10 @@ class tx_caretakerinstance_CommandService
      *
      * The Command Request is validated, decoded and then executed.
      *
-     * @param tx_caretakerinstance_CommandRequest $commandRequest
-     * @return tx_caretakerinstance_CommandResult The command result object
+     * @param CommandRequest $commandRequest
+     * @return CommandResult The command result object
      */
-    public function executeCommand(tx_caretakerinstance_CommandRequest $commandRequest)
+    public function executeCommand(CommandRequest $commandRequest)
     {
         try {
             if ($this->securityManager->validateRequest($commandRequest)) {
@@ -92,14 +99,14 @@ class tx_caretakerinstance_CommandService
                         $results[] = $this->operationManager->executeOperation($operation[0], $operation[1]);
                     }
 
-                    return new tx_caretakerinstance_CommandResult(tx_caretakerinstance_CommandResult::status_ok, $results);
+                    return new CommandResult(CommandResult::status_ok, $results);
                 }
-                return new tx_caretakerinstance_CommandResult(tx_caretakerinstance_CommandResult::status_error, null, 'The request could not be decrypted');
+                return new CommandResult(CommandResult::status_error, null, 'The request could not be decrypted');
             }
-        } catch (tx_caretakerinstance_SecurityManagerException $exception) {
-            return new tx_caretakerinstance_CommandResult(tx_caretakerinstance_CommandResult::status_error, null, 'The request could not be verified (' . $exception->getCode() . ': ' . $exception->getMessage() . ')');
+        } catch (SecurityManagerException $exception) {
+            return new CommandResult(CommandResult::status_error, null, 'The request could not be verified (' . $exception->getCode() . ': ' . $exception->getMessage() . ')');
         }
-        return new tx_caretakerinstance_CommandResult(tx_caretakerinstance_CommandResult::status_error, null, 'The request could not be verified');
+        return new CommandResult(CommandResult::status_error, null, 'The request could not be verified');
     }
 
     /**
@@ -116,10 +123,10 @@ class tx_caretakerinstance_CommandService
     /**
      * Encode / encrypt the command result with the security manager
      *
-     * @param tx_caretakerinstance_CommandResult $commandResult
+     * @param CommandResult $commandResult
      * @return string
      */
-    public function wrapCommandResult(tx_caretakerinstance_CommandResult $commandResult)
+    public function wrapCommandResult(CommandResult $commandResult)
     {
         $json = $commandResult->toJson();
 
