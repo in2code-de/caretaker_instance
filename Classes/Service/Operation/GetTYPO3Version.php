b8214e89b1239a18e7d9f421a87fd2d9acb93726
Andreas Nedbal <andreas.nedbal@in2code.de>
2023-09-02 16:24:53 +0200

    [TASK] Properly namespace Operation classes

---
+++ Classes/Service/Operation/GetTYPO3Version.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Caretaker\CaretakerInstance\Service\Operation;
+
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2009-2011 by n@work GmbH and networkteam GmbH
+ *
+ * All rights reserved
+ *
+ * This script is part of the Caretaker project. The Caretaker project
+ * is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use Caretaker\CaretakerInstance\Entity\Operation\OperationResult;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\SingletonInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * This is a file of the caretaker project.
+ * http://forge.typo3.org/projects/show/extension-caretaker
+ *
+ * Project sponsored by:
+ * n@work GmbH - http://www.work.de
+ * networkteam GmbH - http://www.networkteam.com/
+ *
+ * $Id$
+ */
+
+/**
+ * A Operation which returns the current TYPO3 version
+ *
+ * @author Martin Ficzel <martin@work.de>
+ * @author Thomas Hempel <thomas@work.de>
+ * @author Christopher Hlubek <hlubek@networkteam.com>
+ * @author Tobias Liebig <liebig@networkteam.com>
+ *
+ */
+class GetTYPO3Version implements IOperation, SingletonInterface
+{
+    /**
+     * @param array $parameter None
+     * @return OperationResult the current PHP version
+     */
+    public function execute($parameter = array())
+    {
+        if (defined('TYPO3_version')) {
+            return new OperationResult(true, TYPO3_version);
+        }
+        return new OperationResult(true, GeneralUtility::makeInstance(Typo3Version::class)->getVersion());
+    }
+}
