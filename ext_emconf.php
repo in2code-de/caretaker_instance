984e2ccc24f1c049361801f48dd55d63dd960f4a
Andreas Nedbal <andreas.nedbal@in2code.de>
2024-07-12 17:05:44 +0200

    [TASK] Raise version to 4.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => '',
-  'version' => '4.0.4',
+  'version' => '4.1.0',
   'constraints' =>
   array (
     'depends' =>
