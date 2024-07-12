d1ea9a2acbfc98ea88253e2416b0390ede5c15e6
Andreas Nedbal <andreas.nedbal@in2code.de>
2024-07-12 15:22:15 +0200

    [TASK] Limit version to TYPO3 v12 only
    Related: https://projekte.in2code.de/issues/64998

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,11 +12,11 @@ $EM_CONF[$_EXTKEY] = array (
   'lockType' => '',
   'author_company' => '',
   'version' => '3.0.3',
-  'constraints' => 
+  'constraints' =>
   array (
     'depends' =>
     array (
-      'typo3' => '9.5.0-12.4.99',
+      'typo3' => '12.4.0-12.4.99',
     ),
     'conflicts' =>
     array (
