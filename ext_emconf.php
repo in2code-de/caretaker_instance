999e696fe1b87b5a6f09d2f2eb6258cb32df7f4f
Andreas Nedbal <andreas.nedbal@in2code.de>
2025-05-02 11:01:34 +0200

    [TASK] Initial version raise for v13 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,12 +11,12 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => '',
-  'version' => '5.0.0',
+  'version' => '6.0.0',
   'constraints' =>
   array (
     'depends' =>
     array (
-      'typo3' => '12.4.0-12.4.99',
+      'typo3' => '13.4.0-13.4.99',
     ),
     'conflicts' =>
     array (
